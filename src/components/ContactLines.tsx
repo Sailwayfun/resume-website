1cba85d378c438d61a00766026f3b787fad1856c
sail <sail@chainsecurity.asia>
2025-12-15 11:06:40 +0800

    Migrate to Astro 5 (no React) with /en i18n

--- src/components/ContactLines.tsx
+++
@@ -1,9 +0,0 @@
-interface ContactLinesProps {
-  email: string;
-}
-
-export function ContactLines({ email }: ContactLinesProps) {
-  return (
-    <div className="flex flex-col gap-2 text-[var(--text)] break-all">{email}</div>
-  );
-}
